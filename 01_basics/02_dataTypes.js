68e002793d1ae9e44402c1c1195c86f050b49a7c
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-01-17 11:25:11 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using node js, not browser
+
+console.log(3
+    +
+    3); // Code readability should be high
+
+console.log("Zeyaur")
+
+
+
+let name = "Zeyaur"
+let age = 25
+let isLogedIn = false
+
+// number => 2 to the power 53 range hota hai
+// bigint -> bahat jyada bda number ke liye use hota hai
+// string =>" "
+// boolean => true/false
+// null => standalone value 
+// undefined => it means abhi value assign nahi hai
+// symbol => unique ke liye use hota hai
+
+
+
+// object
+
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+console.log(typeof name); // string
+console.log(typeof empty); // undefined
