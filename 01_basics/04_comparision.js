635e5d904177cdf3363e273d2446792a944cee29
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-01-18 06:27:20 +0000

    Commited

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);  // true
+// console.log(2 >= 1);  // true
+// console.log(2 < 1);  // false
+// console.log(2 == 1);  // false
+// console.log(2 != 1);  // true
+
+
+// console.log("2" > 1);  // true
+// console.log("02" > 1);  // true 
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===    
+
+// console.log("2" == 2); // true  -> yha compare kar raha hai
+// console.log("2" === 2); // false  -> yha pe pahle data type check karega
