c616c1da21370ab959630cd2fa51157de83db0e1
Zeyaur Rahman <147442083+zeyaurrahman786@users.noreply.github.com>
2024-01-22 09:32:46 +0000

    array

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -20,9 +20,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Zeyaur"))
+console.log(Array.from("Zeyaur"))
+console.log(Array.from({name: "Zeyaur"})) // interesting
 
 let score1 = 100
 let score2 = 200
